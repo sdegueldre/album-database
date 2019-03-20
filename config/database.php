fd5c0d075e786b556ef3d165f5c21985a074296e
Samuel Degueldre <samuelfdegueldre@gmail.com>
2019-03-20 09:00:57 +0100

    Parse DATABASE_URL for psql connection on server

--- config/database.php
+++ config/database.php
@@ -1,5 +1,15 @@
 <?php
 
+if(env('APP_ENV', 'server') == 'server'){
+    $url = parse_url(getenv("DATABASE_URL"));
+
+    $host = $url["host"];
+    $port = $url["port"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = ltrim($url["path"], "/");
+}
+
 return [
 
     /*
@@ -61,11 +71,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', $host),
+            'port' => env('DB_PORT', $port),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
